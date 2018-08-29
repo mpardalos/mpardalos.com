c92d94e3526c4e848858799c2ba57b950ba12ebf
Michalis Pardalos <m.pardalos@gmail.com>
2018-08-29 15:52:54 +0300

    Update theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,26 +13,14 @@ MARKDOWN = {
 }
 
 PATH = 'content'
-
 TIMEZONE = 'Europe/London'
-
 DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_RSS = "rss.xml"
 
 BIO = "Functional programming, programming languages and the ramblings of a madman"
 
-# Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
-
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'))
-
-# Social widget
 SOCIAL = (('gitlab', 'https://gitlab.com/michalis_pardalos'),
           ('instagram', 'https://instagram.com/mpgrphoto'))
 
